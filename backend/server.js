acadc01af03ae6811ff797d495b6547121729922
Szyron <143870942+Szyron@users.noreply.github.com>
2025-11-15 00:55:36 +0100

    Shoe Customizer v1.0.0

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-console.log("SMTP_HOST:", process.env.SMTP_HOST);
-console.log("SMTP_USER:", process.env.SMTP_USER);
-console.log("SMTP_PASS:", process.env.SMTP_PASS);
+
 
 const app = express();
 
